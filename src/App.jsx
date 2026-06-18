83875fcaa510bdb944cf460700ccbe1395dc8989
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-06-18 13:07:49 +0200

    conexión del frontend al backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
-import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
